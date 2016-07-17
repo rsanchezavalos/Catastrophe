595843d47cadc8ffda182eb37c5de26422b23bcb
rsanchezavalos <r.sanchezavalos@gmail.com>
2016-07-17 09:31:52 -0500

    Update the python script with some fixes
    - Regex typo in cadaster number
    - Add zoning information
    - Add pyc to gitignore
    - Add a snipet to use Tor as a proxy

--- catastrophe.py
+++ catastrophe.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 __author__      = "rsanchezav"
 
-import urllib2
 import re
+import pdb
+import pandas as pd
 import requests
+import urllib2
+from lxml import html
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from pyvirtualdisplay import Display
-from selenium import webdriver
+import socks
+import socket
+import urllib2
+from TorCtl import TorCtl
 
 class query_catastro(object):
 	"""
@@ -16,10 +20,20 @@ class query_catastro(object):
 	sudo apt-get install xvfb
 
 	How to use it:
-	import catastrophe
-	model = catastrophe.query_catastro()
-	catastro = model.get_account(19.401408,-99.201958)
 
+	import catastrophe	
+	#instanciate class - This function prepares the application and assigns a valid cookie
+	
+	model = catastrophe.query_catastro()	
+	
+	#Having done that you are ready to search for a lat/long
+	
+	catastro = model.get_account(19.401408,-99.201958)
+	
+	#You will receive a pandas DF with zoning and land use information from SEDUVI:
+	#CuentaCatastral: |latitud: |longitud: |Delegacion: |Uso del Suelo 1: |Niveles: 
+	#|Altura: |Zrea Libre: |M2 min. Vivienda: |Densidad: 
+	#|Superficie Maxima de Construccion (Sujeta a restricciones*): |Numero de Viviendas Permitidas:
 	"""
 
 	def prepare_to_request_account(self,LAT,LON):
@@ -99,18 +113,14 @@ class query_catastro(object):
 		f = opener.open(url)
 		text = str(f.read())
 		catastro = re.search("[0-9]{3}_[0-9]{3}_[0-9]{2}",text).group(0)	
-		soup = BeautifulSoup(text,'html.parser')
-		delegacion = soup.find_all('td')[8].getText()
-		cp = soup.find_all('td')[6].getText()
-		colonia = soup.find_all('td')[4].getText()
-		return catastro, delegacion, cp
+		delegacion = re.search("c[A-Z][\w+\s\w+]*",text).group(0)
+		return catastro, delegacion
 
 
 	def get_ficha(self,LAT,LON):
-
 		info = self.get_account(LAT,LON)
 		print "info reached"
-		
+
 		ENDPOINT="/fichasReporte/fichaInformacion.jsp?"		
 		url=self.ROOT_URL + ENDPOINT
 		url+="nombreConexion=" + str(info[1])
@@ -119,19 +129,28 @@ class query_catastro(object):
 		url+="&x=" + str(LON)
 		url+="&y="+ str(LAT)
 		url+="&z=0.5" 
-		print url
 
-		url = "http://ciudadmx.df.gob.mx:8080/seduvi/fichasReporte/fichaInformacion.jsp?nombreConexion=ALVARO OBREGON&cuentaCatastral=037_354&idDenuncia=&ocultar=1&x=-99.201958&y=19.401408&z=0.5"
 		opener = urllib2.build_opener()
 		opener.addheaders.append(('Cookie', 'JSESSIONID=' + self.SESSION_COOKIE))
+		req = requests.get(url)
+		tree = html.fromstring(req.content)
+		var = tree.xpath('//th[@class="zon"]/text()')
+		ans = tree.xpath('//td[@class="zon"]/text()')
+		diccionario_predio = {}
 
-		req = requests.get(url).text
-		print req
-		text = str(req)
-		return text
+		diccionario_predio["CuentaCatastral"] = str(info[0])
+		diccionario_predio["Delegacion"] = str(info[1])
+		diccionario_predio["latitud"] = str(LAT)
+		diccionario_predio["longitud"] = str(LON)
 
+		for v, a in zip(var[:8],ans[:8]):
+			diccionario_predio[v] = a
 
+		#print urllib2.urlopen("http://almien.co.uk/m/tools/net/ip/").read()
+		
+		data = pd.DataFrame(diccionario_predio.values(), index=diccionario_predio.keys()).transpose()
 
+		return data
 
 	def __init__(self):
 		super(query_catastro, self).__init__()
@@ -142,9 +161,21 @@ class query_catastro(object):
 		self.MAP_WIDTH=700
 		self.MAP_HEIGHT=800
 
-		# We need a cookie to make requests effectively
-		display = Display(visible=0, size=(1024, 768))
-		display.start()
-		driver= webdriver.Firefox()
-		driver.get("http://ciudadmx.df.gob.mx:8080/seduvi/")
-		self.SESSION_COOKIE = driver.get_cookies()[0].get("value")
+		# Tor IP.
+		socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS4, '127.0.0.1', 9050, True)
+		socket.socket = socks.socksocket
+		proxy_support = urllib2.ProxyHandler({"http" : "127.0.0.1:8118"})
+		opener = urllib2.build_opener(proxy_support) 
+
+		#self.conn = TorCtl.connect(controlAddr="127.0.0.1", controlPort=9051)
+		#self.proxy_support = urllib2.ProxyHandler({"http" : "127.0.0.1:8118"})
+		#self.conn.send_signal("NEWNYM")
+		#opener = urllib2.build_opener(self.proxy_support) 
+		#urllib2.install_opener(opener)
+		#print "Changing ip address to"  + str((urllib2.urlopen("http://www.ifconfig.me/ip").read()))
+
+		r = requests.get(self.ROOT_URL)
+		if r.status_code == 200:
+			self.SESSION_COOKIE = r.cookies.items()[0][1]
+
+
